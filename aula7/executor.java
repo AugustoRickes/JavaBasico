0a46c23ff19fbf1ff54c604d469a310965c10227
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-07-20 11:05:56 -0300

    equacao, delta e eqxn ok

---
+++ aula7/executor.java
@@ -0,0 +1,11 @@
+package aula7;
+
+import java.util.Scanner;
+
+public class executor extends Equacao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println();
+    }
+
+}
