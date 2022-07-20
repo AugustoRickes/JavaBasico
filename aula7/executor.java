e12b95badc999187459e2676770f8deadd9f60af
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-07-20 12:00:04 -0300

    falta resolver xn e xp no executor

--- aula7/executor.java
+++ aula7/executor.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 
 public class executor extends Equacao {
     public static void main(String[] args) {
-        Equacao e1 = new Equacao();
+        // Equacao e1 = new Equacao();
+        Delta dt = new Delta();
         EqXn xnXp = new EqXn();
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("qual o valor do A?");
-        e1.setA(entrada.nextDouble());
+        dt.setA(entrada.nextDouble());
         System.out.println("qual o valor do B?");
-        e1.setB(entrada.nextDouble());
+        dt.setB(entrada.nextDouble());
         System.out.println("qual o valor do C?");
-        e1.setC(entrada.nextDouble());
+        dt.setC(entrada.nextDouble());
+        dt.calculaDelta();
+        System.out.println("resultado de delta: " + dt.getDelta());
+        if (dt.getDelta() < 0) {
+            System.out.println("a equacao nao possui raizes reais.");
+        } else {
+            System.out.println("xn :" + xnXp.calculaXn(dt.getDelta()));
+            System.out.println("xp :" + xnXp.calculaXP(dt.getDelta()));
+        }
 
     }
 
