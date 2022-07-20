fde080a466332759fe8435a7e662147a0fea5e8d
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-07-20 11:16:47 -0300

    montagem do executor

--- aula7/executor.java
+++ aula7/executor.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 
 public class executor extends Equacao {
     public static void main(String[] args) {
+        Equacao e1 = new Equacao();
+        EqXn xnXp = new EqXn();
+
         Scanner entrada = new Scanner(System.in);
-        System.out.println();
+        System.out.println("qual o valor do A?");
+        e1.setA(entrada.nextDouble());
+        System.out.println("qual o valor do B?");
+        e1.setB(entrada.nextDouble());
+        System.out.println("qual o valor do C?");
+        e1.setC(entrada.nextDouble());
+
     }
 
 }
