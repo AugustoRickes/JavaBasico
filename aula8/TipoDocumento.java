3c46cb85311081ffc1dfd9282ce36b76e289ddbf
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-07-21 14:02:01 -0300

    tipagem enumeradora e tipos de modelagem

---
+++ aula8/TipoDocumento.java
@@ -0,0 +1,19 @@
+package aula8;
+
+public enum TipoDocumento {
+    CPF {
+        @Override
+        public String gerarNumeroTeste() {
+            return GeraCpfCnpj.cpf();
+
+        }
+    },
+    CNPJ {
+        @Override
+        public String gerarNumeroTeste() {
+            return GeraCpfCnpj.cnpj();
+        }
+    };
+
+    public abstract String gerarNumeroTeste();
+}
